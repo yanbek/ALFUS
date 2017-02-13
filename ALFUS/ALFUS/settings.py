49b8f833f9522a7c1b3e5f4a920f5d0d72ddc818
viigleik <vigleikl@gmail.com>
2017-02-13 13:15:55 +0100

    Update settings.py

--- ALFUS/ALFUS/settings.py
+++ ALFUS/ALFUS/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 't%w5q5&_u+iusb_mq%rr0656@n+lo0sgalj&8dz62s-(u3z(xa'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
