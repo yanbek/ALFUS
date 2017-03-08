b2401f168f670eaeb0cc21ac5295e1668d093d0a
Yanzhe Bekkemoen <yanzheb@stud.ntnu.no>
2017-03-08 15:55:26 +0100

    Small template color bug fix

--- ALFUS/personal/urls.py
+++ ALFUS/personal/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^contact/$', views.contact, name='contact'),
     # url(r'^user/(\w+)/$', views.profile, name="profile"),
-    url(r"^login/$", views.login_view, name='login'),
+    url(r"^login/", views.login_view, name='login'),
     url(r"^register/$", views.reqister_view, name="register"),
 ]
--- ALFUS/questions/urls.py
+++ ALFUS/questions/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from . import views
+from personal import views as p_views
 
 urlpatterns = [
     # ex: /questions/
     url(r'^$', views.index, name='index'),
     url(r'^(?P<question_id>\d+)/$', views.detail, name='detail'),
     url(r'^(?P<question_id>[0-9]+)/results/$', views.answer, name='answer'),
-    #url(r"^login/$", views.logout_view, name='logout'),
+    url(r"^logout/", p_views.logout_view, name='logout'),
 ]
