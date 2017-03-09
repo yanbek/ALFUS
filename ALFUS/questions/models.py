886c6815b967b580ba4faca3191c155d4b26fbb0
mariesva <mariesva@stud.ntnu.no>
2017-03-09 15:15:20 +0100

    added topic
    Removed date and added topic under the Question model.

--- ALFUS/questions/models.py
+++ ALFUS/questions/models.py
@@ -7,18 +7,17 @@ from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    topic_text = models.CharField(max_length=200,default="x")
+
 
     def __str__(self):
         return self.question_text
 
-    def was_published_recently(self):
-        now = timezone.now()
-        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+    def topic(self):
+        return self.topic_text
 
-    was_published_recently.admin_order_field = 'pub_date'
-    was_published_recently.boolean = True
-    was_published_recently.short_description = 'Published recently?'
+    topic.short_description = 'Sort topic'
+    topic.admin_order_field = 'topic_text'
 
 
 class Choice(models.Model):
