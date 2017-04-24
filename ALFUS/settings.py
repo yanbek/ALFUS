243548cbff637333b269b314488d45e5728afb29
Yanzhe Bekkemoen <yanzheb@stud.ntnu.no>
2017-04-24 22:39:33 +0200

    Fix url error

--- ALFUS/settings.py
+++ ALFUS/settings.py
@@ -27,6 +27,10 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '138.68.180.101', 'alfus.me', 'www.alfus.me']
 
+CRON_CLASSES = [
+    "questions.cron.Difficulty_adjustment",
+    # ...
+]
 
 # Application definition
 
@@ -69,7 +73,8 @@ ROOT_URLCONF = 'ALFUS.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')]
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -105,6 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
     },
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 9,
+        }
     },
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
