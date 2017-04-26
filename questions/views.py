137966554012c98b8aed4f2108822b73dbac9bf4
Yanzhe Bekkemoen <yanzheb@stud.ntnu.no>
2017-04-26 10:38:49 +0200

    Fix change password bug

--- questions/views.py
+++ questions/views.py
@@ -127,10 +127,7 @@ def change_password(request):
         if form.is_valid():
             user = form.save()
             update_session_auth_hash(request, user)
-            messages.success(request, "Your password was successfully updated!")
             return redirect('/questions/profile')
-        else:
-            messages.error(request, "Please correct the error below.")
     else:
         form = PasswordChangeForm(request.user)
     return render(request, 'questions/change_password.html', {"form": form})
